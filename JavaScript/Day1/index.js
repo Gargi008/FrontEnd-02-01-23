cbeeef61b9c4f889c520a824771104b6b7d495ac
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2023-01-30 23:02:20 +0530

    Create index.js

---
+++ JavaScript/Day1/index.js
@@ -0,0 +1,63 @@
+console.log("hello JS") // 010101010110000
+
+
+
+/* JS is a programming language
+JS is a dynamic
+JS is weakly typed 
+JS is a compiled at the run time
+
+
+LiveScript
+
+
+js engine - browers have js engine built into them.
+
+1. parses ur code
+2. conerts that to its equivalent machine code
+3. excute machine code.
+
+chrome - v8
+firefox -spidermonkey
+safary - javascriptcore
+edge - chakra
+
+
+
+
+Dynamic interpreted prog langugae
+
+for js code is not precompiled -
+code evaluated and executed at the runtime
+u can change the code the code runtime. like u can chnge the type of a variable at the runtime.
+
+
+
+weakly typed language
+datatypes r not defined , it is not defined that a variable should a certain kind of value
+datatypes can be changed
+let a="g"
+a=1
+
+server side and browser 
+
+. JS - it can run the code in a browser
+
+
+browser side
+using js could excute the code in the browser
+.
+*/
+console.log("hola")
+
+
+/* server side 
+
+js - outside the browser
+v8 js was extracted to run js outside te browser - node js
+
+
+
+
+
+*/
