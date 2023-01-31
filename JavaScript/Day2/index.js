24a54595bb5885a28582c75bfc2ccc906bf3c896
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2023-01-31 21:38:19 +0530

    Add files via upload

---
+++ JavaScript/Day2/index.js
@@ -0,0 +1,84 @@
+console.log("Finallyyyy let's start coding!!!!")
+
+
+/* primitive datatypes
+
+string
+number 
+bigint
+boolean
+undefined
+symbol
+null
+
+
+1,2,3,4,5
+true , false - boolean
+
+
+gargi, students, you , a , b, c, d
+
+
+20 bananas
+10 apples
+15 guava
+
+*/
+
+/* 
+
+variable - labled box/ bags
+
+inside a variable we ae storing the values amd we are giving it a name,
+sothat we can use it afterwards by refering to that name
+
+*/
+
+let apples= '20'; //string
+apples='20' //number
+let bananas= 25
+console.log('no of apples---------------------------',apples)
+console.log('narayan ---typeof no of apples  ---->',typeof(apples),apples) // typeof is a unary operator
+console.log('no of bananas',bananas)
+console.log('no of bananas',typeof(bananas),bananas)
+
+
+
+const devendra= "hello"
+/* devendra="holaaa" */
+console.log("devendra",devendra)
+
+
+console.log("------------------------------------------------------")
+let myName= "Gargi"   /* js is a case sensitive */  /* camel casing */
+console.log(myName)
+
+/* in js a variable name can't start with a number */ //let 1myName= "Gargi" 
+
+/* in js variables can start with $, _ */
+
+let $gargi="!richi rich"
+let _gargi="!richi rich"
+let _lokesh="good boy!"
+console.log(_lokesh)
+
+console.log($gargi)
+console.log(_gargi)
+/* in js in variable names spaces r not allowed */
+let myNewName= "Gargi" 
+console.log(myName)
+
+
+/* let const = apples; */ // not allowed
+let gargiPassed= true // boolean
+let gargiFailed= false // boolean
+console.log(typeof(gargiPassed))
+console.log(typeof(gargiFailed))
+
+let $$=1
+console.log($$)
+
+/* bala krishna is a good boy */
+
+let balaKrishnaIsAGoodBoy= true
+console.log(balaKrishnaIsAGoodBoy)
