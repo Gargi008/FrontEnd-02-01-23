0786f87d71d45d2ebf03863565048945353f67e5
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2023-03-17 23:54:16 +0530

    Add files via upload

---
+++ JavaScript/Day33 - 2.0/script.js
@@ -0,0 +1,9 @@
+/* $('.carousalContainer').slick(); */
+$('.carousalContainer').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    //dots: true,
+  });
